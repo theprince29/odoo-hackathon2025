09df08d95d623cc1fc1dff1c6cf6ef7b102d56de
Shivam <sg641818@gmail.com>
2025-07-12 13:00:52 +0530

    feat: update admin routes to include product retrieval and approval status updates

--- backend-reWear/routes/productRoutes.js
+++ backend-reWear/routes/productRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import { upload, createProduct, updateProduct, deleteProduct, getAllProducts, updateProductApprovalStatus,buyProduct } from '../controllers/productController.js';
+import { upload, createProduct, updateProduct, deleteProduct, buyProduct } from '../controllers/productController.js';
 import { protect } from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/', protect, upload, createProduct);
 router.put('/:id', upload, updateProduct);
 router.delete('/:id', deleteProduct);
-router.get("/allProduct", getAllProducts)
-router.patch('/:id/approval', updateProductApprovalStatus);
+
 router.post('/buyProduct', protect, buyProduct )
 
 export default router;
